7e74117d84faecd4b13a2aedd536b5fc1973389d
Chehadeh Flitty <flitty.chehadeh@gmail.com>
2023-08-05 15:19:54 +0300

    <FE> created the contact list to show all contacts

--- frontend/contact-manager-system/src/App.jsx
+++ frontend/contact-manager-system/src/App.jsx
@@ -1,29 +1,15 @@
 import { useState } from "react";
-import ContactForm from "./components/ContactForm/ContactForm";
+// import ContactForm from "./components/ContactForm/ContactForm";
 import "./styles/App.css";
 import ContactList from "./components/ContactList/ContactList";
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    {
-      name: "walid",
-      phone_number: "+96170123456",
-      latitude: 13,
-      longitude:12,
-    },
-    {
-      name: "sarah",
-      phone_number: "+96170321654",
-      latitude: 14,
-      longitude:18,
-    },
-    
-  ]);
+  const [contacts] = useState([]);
 
   return (
     <div className="flex">
-      {/* <ContactForm setContacts={setContacts} contacts={contacts} />
-      <ContactList contacts={contacts} /> */}
+      {/* <ContactForm setContacts={setContacts} contacts={contacts} /> */}
+      <ContactList contacts={contacts} />
       
     </div>
   );
--- frontend/contact-manager-system/src/components/ContactList/ContactList.jsx
+++ frontend/contact-manager-system/src/components/ContactList/ContactList.jsx
@@ -1,8 +1,30 @@
-import React from "react";
 import "./ContactList.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ContactCard from "../ContactCard/ContactCard";
 
 const ContactList = () => {
- 
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/getContacts')
+      .then(response => setContacts(response.data))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div className="contact-list">
+      {contacts.map((contact) => (
+        // <ContactCard key={contact.id} contact={contact} />
+        <ContactCard 
+        name={contact.name}
+        phone_number={contact.phone_number}
+        latitude={contact.latitude}
+        longitude={contact.longitude} />
+      ))}
+    </div>
+  );
 };
 
 export default ContactList;
+
